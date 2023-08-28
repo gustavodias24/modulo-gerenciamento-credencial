9d6bafc87d44eca30092bcd78138ec776358a3d2
benicio <gustavo024dias@gmail.com>
2023-08-28 15:18:36 -0300

    dialog carregando

--- app/src/main/java/com/benicio/admkey/util/RetrofitUtil.java
+++ app/src/main/java/com/benicio/admkey/util/RetrofitUtil.java
@@ -1,5 +1,11 @@
 package com.benicio.admkey.util;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+
+import com.benicio.admkey.databinding.CarregarLayoutBinding;
 import com.benicio.admkey.service.Service;
 
 import retrofit2.Retrofit;
@@ -7,6 +13,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitUtil {
 
+    public static Dialog criarDialogCarregando(Activity c){
+        AlertDialog.Builder b = new AlertDialog.Builder(c);
+        b.setView(CarregarLayoutBinding.inflate(c.getLayoutInflater()).getRoot());
+        return b.create();
+    }
     public static Retrofit criarRetrofit(){
         return new Retrofit.Builder()
                 .baseUrl("")
